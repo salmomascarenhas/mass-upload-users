84768caa4ee0148563c1d8789ee80efc019c1f83
salmomascarenhas <salmo.cruz@gmail.com>
2025-03-18 11:31:02 -0300

    Rename getHello to healthCheck and update response message; include UsersModule and WhitelabelsModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './nestjs-modules/config/config.module';
 import { DatabaseModule } from './nestjs-modules/database/database.module';
+import { UsersModule } from './nestjs-modules/users/users.module';
+import { WhitelabelsModule } from './nestjs-modules/whitelabel/whitelabels.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    WhitelabelsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
