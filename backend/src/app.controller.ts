83875c3f580e6aab807c6c7dcf3bea589b91e99d
salmomascarenhas <salmo.cruz@gmail.com>
2025-03-19 11:01:24 -0300

    Add Swagger API operation for health check endpoint

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({
+    summary: 'Health Check',
+    description: 'Rota para verificar o status da API',
+  })
   @Get()
   healthCheck(): string {
     return this.appService.healthCheck();
