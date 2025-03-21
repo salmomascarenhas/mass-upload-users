dfcaa9bc96aad1316ba405a9f13e0df312db4434
salmomascarenhas <salmo.cruz@gmail.com>
2025-03-20 23:50:43 -0300

    create socket.ts

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,10 +1,11 @@
 import { io, Socket } from "socket.io-client";
 
 let socket: Socket;
+const apiURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
 export function connectToSocket(flowId: string) {
   if (!socket || !socket.connected) {
-    socket = io("http://localhost:3000", {
+    socket = io(`${apiURL}`, {
       path: "/socket.io",
       query: { flowId },
     });
